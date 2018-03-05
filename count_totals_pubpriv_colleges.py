de59ba7dd53ca09954af77461cb6c35dadf11d9c
Richard Decal <richard.decal@ncf.edu>
2018-03-05 09:02:55 -0500

    counted totals for priv and pub colleges

--- count_totals_pubpriv_colleges.py
+++ count_totals_pubpriv_colleges.py
@@ -1,23 +1,32 @@
 from mrjob.job import MRJob
 
 
-
-class CountTotalColleges(MRJob):
+class Count_College_Types(MRJob):
     '''
-    python count_total_colleges.py Data/colleges.csv
+    python count_totals_pubpriv_colleges.py Data/colleges.csv
 
-    Output: "total colleges:"       1303
-
+    "total public colleges:"        470
+    "total private colleges:"       832
     '''
+
     def mapper(self, _, line):
-        yield ('_', 1)
+        fields = line.split(',')
+        try:
+            pubpriv = int(fields[2])
+        except:
+            print("exception", fields)
+            # used to find "Montana State University, Northern" with unexpected extra comma
+        if pubpriv == 1:
+            yield ('public', 1)
+        else:
+            yield ('private', 1)
 
-    def reducer(self, _, values):
+    def reducer(self, school_type, values):
         count = 0
         for v in values:
             count += 1
-        yield ('total colleges:', count)
+        yield ('total {} colleges:'.format(school_type), count)
+
 
 if __name__ == '__main__':
-    result = CountTotalColleges.run()
-    print(result)
+    result = Count_College_Types.run()
