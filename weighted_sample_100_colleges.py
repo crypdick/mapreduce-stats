b20cc263e413c22ea4e8047e1d0f9a4c820c74c9
Richard Decal <richard.decal@ncf.edu>
2018-03-05 09:12:22 -0500

    finished implementing

--- weighted_sample_100_colleges.py
+++ weighted_sample_100_colleges.py
@@ -2,28 +2,31 @@ from mrjob.job import MRJob
 from numpy.random import uniform
 
 
-'''
-first, count total in sample
-then, select w that probability 100/total
-TODO: weigh the sampling
-'''
 
 class WeightedSample100(MRJob):
     def mapper_init(self):
-        self.desired_samples = 100
-        self.n_colleges = 1303  # result from count_total_colleges.py
+        self.expected_n_per_pubpriv = 50
+        # result from count_totals_pubpriv_colleges.py
+        self.n_pub = 470
+        self.n_priv = 832
 
     def mapper(self, _, line):
         sample = uniform()
-        if sample <= self.desired_samples/self.n_colleges:
-            yield ('_', line)
+        fields = line.split(',')
+        pubpriv = int(fields[2])
+        if (pubpriv == 1 and
+            sample <= self.expected_n_per_pubpriv/self.n_pub):
+            yield ('public', line)
+        elif (pubpriv == 2 and
+              sample <= self.expected_n_per_pubpriv / self.n_priv):
+            yield ('private', line)
 
-    def reducer(self, key, values):
+    def reducer(self, college_type, values):
         total_samples = 0
         for value in values:
             total_samples +=1
-            yield ('Uniformly sampled college: ', value)
-        yield ('Total samples: ', total_samples)
+            yield ('{} college: '.format(college_type), value)
+        yield ('Total {} colleges sampled: '.format(college_type), total_samples)
 
 if __name__ == '__main__':
     WeightedSample100.run()
