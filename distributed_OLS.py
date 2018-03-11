a0bec216e10426a44601cb643f5d2288c18cfc83
Richard Decal <richard.decal@ncf.edu>
2018-03-11 15:04:14 -0400

    got mapper and reducer mostly written

--- distributed_OLS.py
+++ distributed_OLS.py
@@ -1,48 +1,42 @@
 # 2. Calculate the variance in electricity prices among the states.
 from mrjob.job import MRJob
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from numpy.random import choice
 
 
-class VarianceElectricityCost(MRJob):
+class MrLeastSquares(MRJob):
     '''
     Run with:
-    python variance.py < Data/Electricity.csv
+    python distributed_OLS.py < Data/states.csv
 
-    Variance formula is E[X^2] - E[X]^2
+    fit Population = Area * alpha + beta
+
+
+    State data format: Name, Abbreviation, Area (Sq. Miles), Population
 
     Output:
     "Variance"	71.3836895496527
     '''
     def mapper(self, _, line):
-        _, cost = line.split(',')
-        # Electricity : State, Price per Kilowatt Hour
-        #try:
-        cost = float(cost)
-        yield ('bucket', (cost, cost**2, 1))
-        #except ValueError:  # some empty fields
-         #   pass
+        words = line.split(',')
+        _, _, _, area, population = words
+        # use int to solve serialization exception
+        # https://stackoverflow.com/a/11942689/4212158
+        bucket_id = int(choice(range(5)))
+        yield (bucket_id, (int(area), int(population)))
 
-    def combiner(self, key, values):
-        n = 0
-        sum_x_squared = 0
-        sum_x = 0
-        for cost, costsq, count in values:
-            n += count
-            sum_x += cost
-            sum_x_squared += costsq
-
-        yield ("bucket", (sum_x, sum_x_squared, n))
-
-    def reducer(self, key, values):
-        n = 0
-        sum_x_squared = 0
-        sum_x = 0
-        for sum_cost, sum_costsq, count in values:
-            n += count
-            sum_x += sum_cost
-            sum_x_squared += sum_costsq
-
-        variance = sum_x_squared/n - (sum_x/n**2)**2
-        yield ("Variance", variance)
+    def reducer(self, bucked_id, values):
+        areas, populations = zip(*values)
+        print("areas ", areas)
+        print("pop", populations)
+        areas, populations = np.array(areas).reshape(-1, 1),\
+                             np.array(populations).reshape(-1, 1),
+        #areas, populations = areas.reshape(-1, 1), populations.reshape(-1, 1)
+        try:
+            coefficients = LinearRegression(fit_intercept=False).fit(areas, populations).coef_
+        except ValueError:
+            print(coefficients)
 
 if __name__ == '__main__':
-    VarianceElectricityCost.run()
+    MrLeastSquares.run()
