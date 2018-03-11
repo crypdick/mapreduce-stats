80aaf8c61b350cc70aa6f4be658b04eb32c20f09
Richard Decal <richard.decal@ncf.edu>
2018-03-11 15:37:23 -0400

    added second reducer, coefficient extraction fixed

--- distributed_OLS.py
+++ distributed_OLS.py
@@ -1,5 +1,6 @@
 # 2. Calculate the variance in electricity prices among the states.
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from numpy.random import choice
@@ -18,6 +19,12 @@ class MrLeastSquares(MRJob):
     Output:
     "Variance"	71.3836895496527
     '''
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper,
+                       reducer=self.regression_reducer),
+                MRStep(reducer=self.average_coeffs_reducer)]
+
     def mapper(self, _, line):
         words = line.split(',')
         _state, _, _, area, population = words
@@ -31,16 +38,30 @@ class MrLeastSquares(MRJob):
             # solve by deleting them
             print(_state, area, population)
 
-    def reducer(self, bucked_id, values):
+    def regression_reducer(self, bucket_id, values):
         areas, populations = zip(*values)
+        n_samples_this_reducer = len(areas)
         # make compatible with scikit
         areas, populations = np.array(areas).reshape(-1, 1),\
                              np.array(populations).reshape(-1, 1),
         #areas, populations = areas.reshape(-1, 1), populations.reshape(-1, 1)
-        try:
-            coefficients = LinearRegression(fit_intercept=False).fit(areas, populations).coef_
-        except ValueError:
-            print(coefficients)
+        lin_model = LinearRegression(fit_intercept=True).fit(areas, populations)
+        alpha = lin_model.coef_[0][0]  # extract alpha from array
+        intercept = lin_model.intercept_[0]
+        #print(alpha, intercept, lin_model.coef_)
+        yield ('_', ([alpha, intercept], n_samples_this_reducer))
+
+    def average_coeffs_reducer(self, _, values):
+        print("hi!!!!!!")
+        coefficients = []
+        num_samples = []
+        print(type(values))
+        for (coeff, n_samp) in np.nditer(values):
+            coefficients.append(coeff)
+            num_samples.append(n_samp)
+        coeff_weighted_avg = np.average(coefficients, weights=num_samples)
+        print(coeff_weighted_avg)
+
 
 if __name__ == '__main__':
     MrLeastSquares.run()
